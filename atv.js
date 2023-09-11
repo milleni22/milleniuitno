82fad24527b49acadc190633cabebe6201dbc5e8
milleni22 <142020875+milleni22@users.noreply.github.com>
2023-09-11 11:17:10 -0400

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,57 @@
+class Animal {
+    constructor(nome, cor) {
+        this.nome = nome;
+        this.cor = cor;
+    }
+
+    falar() {
+        return `Olá, meu nome é ${this.nome}`;
+    }
+}
+
+//criando uma classe derivada de Animal
+class Cachorro extends Animal {
+    constructor(nome, cor, raça) {
+        super(nome, cor);
+        this.raça = raça;
+    }
+    
+    latir() {
+    return `au au!`
+}
+}
+
+class Gato extends Animal {
+    constructor(nome, cor, raça) {
+        super(nome, cor);
+        this.raça = raça;
+    }
+
+    miar() {
+        return `miau!`;
+    }
+}
+
+// criando um array de animais
+const animais = [
+    new Cachorro("Rex", "Marrom", "Pastor Alemâo"),
+    new Gato("Mia", "Preto", "Siamês"),
+];
+
+//percorrendo o array e imprimindo o nome e a cor de cada animal
+for (const animal of animais) {
+    console.log(`O animal "${animal.nome}" é ${animal.cor}.`);
+}
+
+//chamando o método "falar()" em cada animal
+for (const animal of animais) {
+    console.log(animal.falar());
+}
+
+//chamando o método "latir()" no cachorro
+const cachorro = animais[0];
+console.log(cachorro.latir());
+
+//chamando o método "miar()" no gato
+const gato = animais[1];
+console.log(gato.miar());
